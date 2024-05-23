d6aa258c15a73f54d9513b4ac82c4ebe9ddd453b
Анна Сеславинская <anna-seslavinskaya@mail.ru>
2024-05-23 17:29:37 +0300

    added product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR,"#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR,"#register_form")
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR,".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR,".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR,".product_main p.price_color")
+    SUCCESS_ALERT = (By.CSS_SELECTOR, ".alert-success")
+    INFO_ALERT = (By.CSS_SELECTOR, ".alert-info")
